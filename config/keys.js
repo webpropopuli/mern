d050597cde74b31de50384dac5419a34c7235232
webpropopuli <webpropopuli@gmail.com>
2019-02-05 09:47:04 -0500

    Working version, prepping for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,2 @@
-module.exports = {
-  mongoURI: "mongodb://WPP:M!Jhereg1m@ds151612.mlab.com:51612/wppsocial",
-  secretOrKey: "1secret0"
-};
+if (process.env.NODE_ENV === "production") module.exports = require("./keys_prod");
+else module.exports = require("./keys_dev");
